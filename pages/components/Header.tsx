c103aeef0c318b2b507cface02a13f18fa80287b
michinoins <m1i1k2i1ya@gmail.com>
2023-05-22 23:13:11 -0700

    add: responsive header

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -1,10 +1,29 @@
-import React from "react";
-import { AppBar, Toolbar, Typography, IconButton, Button } from "@mui/material";
+import React, { useState } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Button,
+  Drawer,
+} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import styled from "@emotion/styled";
 
 const StyledAppBar = styled(AppBar)`
   background-color: #333;
+
+  @media (max-width: 768px) {
+    .desktopMenu {
+      display: none;
+    }
+  }
+
+  @media (min-width: 769px) {
+    .mobileMenu {
+      display: none;
+    }
+  }
 `;
 
 const Title = styled(Typography)`
@@ -19,25 +38,45 @@ const scrollToSection = (id: string) => {
 };
 
 const Header: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <StyledAppBar position="fixed">
       <Toolbar>
         <Title variant="h6">Portfolio</Title>
-        <Button color="inherit" onClick={() => scrollToSection("about")}>
-          About
-        </Button>
-        <Button
+        <IconButton
+          className="mobileMenu"
+          edge="start"
           color="inherit"
-          onClick={() => scrollToSection("education&experience")}
+          aria-label="menu"
+          onClick={handleDrawerToggle}
         >
-          Education&Experience
-        </Button>
-        <Button color="inherit" onClick={() => scrollToSection("projects")}>
-          Projects
-        </Button>
-        <Button color="inherit" onClick={() => scrollToSection("articles")}>
-          Articles
-        </Button>
+          <MenuIcon />
+        </IconButton>
+        <Drawer anchor="right" open={isOpen} onClose={handleDrawerToggle}>
+          {/* ここにモバイルメニューの内容を記述 */}
+        </Drawer>
+        <div className="desktopMenu">
+          <Button color="inherit" onClick={() => scrollToSection("about")}>
+            About
+          </Button>
+          <Button
+            color="inherit"
+            onClick={() => scrollToSection("education&experience")}
+          >
+            Education&Experience
+          </Button>
+          <Button color="inherit" onClick={() => scrollToSection("projects")}>
+            Projects
+          </Button>
+          <Button color="inherit" onClick={() => scrollToSection("articles")}>
+            Articles
+          </Button>
+        </div>
       </Toolbar>
     </StyledAppBar>
   );
